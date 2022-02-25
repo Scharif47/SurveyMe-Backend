6b64d4238f803e18cf3019a9e8a508347e08a2d8
Scharif47 <scharif47@outlook.de>
2022-02-25 21:14:05 +0100

    google oauth added

---
+++ README.md
@@ -0,0 +1,10 @@
+# Welcome to SurveyMe
+
+### This application helps to generate user feedback for services.
+
+
+## Tech used:
+  - node.js, express.js
+  - heroku
+  - google OAuth
+  - passport.js
--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require("express");
-const res = require("express/lib/response");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config({ path: "./utils/.env" });
 
+// save express functionality in app
 const app = express();
 
-const PORT = process.env.PORT || 8000
+// use passport with the google strategy for google logins
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token: ", accessToken);
+      console.log("refresh token: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
 
-app.get("/", (req, res) => {
-  res.send({ Hello: "Friend" });
+// set PORT variable
+const PORT = process.env.PORT || 4000;
+
+// get token from google oauth
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+// come back with valid token
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
 });
-
-
-app.listen(PORT);
